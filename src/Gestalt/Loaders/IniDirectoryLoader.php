d74048f724ec6d5eac152fe1edd0ed08880d705f
Nathan Fiscaletti <nathhan.fiscaletti@gmail.com>
2016-09-07 11:47:36 -0700

    Fixed IniDirectoryLoader

--- src/Gestalt/Loaders/IniDirectoryLoader.php
+++ src/Gestalt/Loaders/IniDirectoryLoader.php
@@ -1,2 +1,47 @@
 <?php
 
+namespace Gestalt\Loaders;
+
+use DirectoryIterator;
+
+class IniDirectoryLoader implements LoaderInterface
+{
+    /**
+     * The directory to load INI configuration files from.
+     *
+     * @var array
+     */
+    protected $directory;
+
+    /**
+     * Create an IniDirectoryLoader instance.
+     *
+     * @param string $directory
+     */
+    public function __construct($directory)
+    {
+        $this->directory = $directory;
+    }
+
+    /**
+     * Load the configuration items and return them as an array.
+     *
+     * @return array
+     */
+    public function load()
+    {
+        $items = [];
+        $directory = new DirectoryIterator(realpath($this->directory));
+
+        foreach ($directory as $file) {
+            if ($file->isFile() && $file->getExtension() == 'ini') {
+                $filename = $file->getFilename();
+                $config = substr($filename, 0, strrpos($filename, '.'));
+
+                $items[$config] = parse_ini_file($filename, true);
+            }
+        }
+
+        return $items;
+    }
+}
