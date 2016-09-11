be727bc6b94591dacb0386405c9a4108e2aa8cf3
Sam Rapaport <sam@gigasavvy.com>
2016-09-10 17:37:07 -0700

    Add new `TestCase` class for testing.

---
+++ tests/TestCase.php
@@ -0,0 +1,76 @@
+<?php
+
+use Gestalt\Configuration;
+
+abstract class TestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Default configuration items to test with.
+     *
+     * @var array
+     */
+    private $configurationItems = [
+        'app' => [
+            'version' => '1.0',
+            'debug' => true,
+            'locale' => 'en',
+        ],
+        'database' => [
+            'default' => 'mysql',
+            'drivers' => [
+                'mysql' => [
+                    'database' => 'gestalt',
+                    'username' => 'sam',
+                    'password' => 'angostura',
+                ],
+                'sqlite' => [
+                    'database' => 'gestalt',
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * Get the test configuration items.
+     *
+     * @return array
+     */
+    protected function getConfigurationItems()
+    {
+        return $this->configurationItems;
+    }
+
+    /**
+     * Get a mocked \Gestalt\Util\ObserverInterface implementation.
+     *
+     * @param  integer $maxUpdates
+     * @param  integer $minUpdates
+     * @return \Mockery
+     */
+    protected function getObserver($maxUpdates = 0, $minUpdates = 1)
+    {
+        $observer = Mockery::mock('Gestalt\Util\ObserverInterface');
+
+        if ($maxUpdates > 0) {
+            $observer->shouldReceive('update')
+                    ->atLeast()
+                    ->times($minUpdates)
+                    ->atMost($maxUpdates)
+                    ->times($maxUpdates);
+        } else {
+            $observer->shouldReceive('update')->atLeast()->times($minUpdates);
+        }
+
+        return $observer;
+    }
+
+    /**
+     * Return test suite to its original state.
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        Mockery::close();
+    }
+}
